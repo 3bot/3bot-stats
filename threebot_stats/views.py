9a39051c5a945cdf56df6824ee23f8512edf51f9
Walter Renner <renner@arteria.ch>
2015-08-05 08:58:53 +0200

    additions for last commit

--- threebot_stats/views.py
+++ threebot_stats/views.py
@@ -1,8 +1,10 @@
-from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from threebot.models import Workflow
-from threebot_stats.utils import *
+from threebot_stats.utils import response_time_series, count_logs, impact
+from threebot.utils import get_my_orgs
 
 
 def index(request):
@@ -10,6 +12,9 @@ def index(request):
 
 
 def detail(request, workflow_slug):
+    orgs = get_my_orgs(request)
+    workflow = get_object_or_404(Workflow, owner__in=orgs, slug=workflow_slug)
+    graph_items = request.GET.get('graph-items', 10)
 
     data = {
         'num_logs': count_logs(workflow),
